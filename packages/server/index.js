c0ed3146f87b0f5874ad8fcb20894a61e8e7c246
Michael <contato@dgmike.com.br>
2021-11-11 03:53:16 -0300

    feat(server): debug log added

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,11 +1,18 @@
+import { debug } from 'util';
 import Koa from 'koa';
 
+const debugLog = debug('app:server');
+
 export const createServer = async () => {
+  debugLog('creating server');
   const app = new Koa();
+
   app.use((ctx) => {
     ctx.body = {
       ok: true,
     };
-  })
+  });
+
+  debugLog('server created');
   return app;
 };
